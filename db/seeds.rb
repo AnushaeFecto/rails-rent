9bd38b1c72068cf4aaf6a6b6aed108fee781d580
Anushae Fecto <anushaefecto@gmail.com>
2018-02-27 17:07:46 +0000

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,8 +23,8 @@ end
   user_id: (1..9).to_a.sample)
 end
 
-amy = User.create!(name: "Amy", photo: "string", address:"blabla", email: "amy@email.com", password: "123123")
-item = Item.new({user_id: amy.id, size: 8, price: 10.0, category:"cocktail dress", description: "pretty party dress"})
+# amy = User.create!(name: "Amy", photo: "string", address:"blabla", email: "amy@email.com", password: "123123")
+item = Item.new({user_id: 1, size: 8, price: 10.0, category:"cocktail dress", description: "pretty party dress"})
 url = 'https://images.unsplash.com/photo-1442810030476-6d83b45a1094?ixlib=rb-0.3.5&s=07dd521ed8eef8af35c3ecae598208ce&auto=format&fit=crop&w=752&q=80'
 item.remote_photo_url = url
 item.save!
