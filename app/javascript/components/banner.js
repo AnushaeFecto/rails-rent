d3c5110651aa730ad4eb03fd184f5661c6b465a9
Velina Cai <velina_30@yahoo.com>
2018-02-27 13:52:55 +0000

    homepagefin

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -3,7 +3,7 @@ import Typed from 'typed.js';
 function loadDynamicBannerText() {
   new Typed('#banner-typed-text', {
     strings: ["Find Your Dream Dress", "Rent It Today"],
-    typeSpeed: 130,
+    typeSpeed: 150,
     loop: true
   });
 }
