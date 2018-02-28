0d8ab88404dc91be9e9304b8d71ada764932a794
Anushae Fecto <anushaefecto@gmail.com>
2018-02-28 15:24:40 +0000

    hellloooo

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,9 @@
 class Item < ApplicationRecord
+  CATEGORIES = ["dress", "shoes", "bags", "shirts"]
+
   belongs_to :user
   has_many :rentals, dependent: :destroy
   validates :description, :price, :size, presence: :true
+  validates :category, presence: true, inclusion: { in: CATEGORIES}
   mount_uploader :photo, PhotoUploader, :mount_on => :photo
 end
